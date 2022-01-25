ce1f76934a627a04bc61b70f785fae83342e882f
Danny Khorasanizadeh <dannykhorasanizadeh@ec.iut.ac.ir>
2022-01-25 21:55:25 +0330

    Fix bug and Add Boy Optimal and Girl Optimal Strategies

--- suitable_maching/main.py
+++ suitable_maching/main.py
@@ -1,131 +1,87 @@
-# number of boys and girls
-boys_num = int(input('Enter number of boys: '))
-girls_num = int(input('Enter number of girls: '))
+class Proposer:
+    def __init__(self, index, preference_order):
+        self.index = index
+        self.order = sort_preferences(preference_order)
+        self.married = False
 
-# lists for saving their prefre
-girls = []
-boys = []
-relation = [[] for x in range(girls_num)]
-
-
-# show index of boys in order ascending
-def sort_boys(boys_arr):
-    return sorted(range(boys_num), key=boys_arr.__getitem__)
-
-
-# show index of girls in order ascending
-def sort_girls(girls_arr):
-    return sorted(range(girls_num), key=girls_arr.__getitem__)
-
-
-class Boy:
-
-    def __init__(self, x, girls_order) -> None:
-        self.x = x
-        self.order = sort_girls(girls_order)
-        self.state = 0
-
-    def compare_girls(self): #we should use another array for girls-opt
-        tmp = -1
-        for i in self.order:
-            if i in relation[self.x]:
-                tmp = i
-                break
-        
-        for i in relation[self.x]:
-            if i != tmp:
-                boys[i].reject()
-
-            relation[self.x].remove(i)
-        
-        relation[self.x].append(tmp)
-    
-    def choose_girl(self):
+    def choose_proposee(self):
         return self.order[0]
-    
-    def reject(self):
-        tmp = self.order.pop(0)
-        self.state = 0
-    
-    def request(self):
-        self.state = 1
-        relation[self.choose_girl()].append(self.x)
 
-
-class Girl:
-
-    def __init__(self, x, boys_order) -> None:
-        self.x = x
-        self.order = sort_boys(boys_order)
-        self.state = 0 # in relation or not
-    
-    def compare_boys(self):
-        tmp = -1
-        for i in self.order:
-            if i in relation[self.x]:
-                tmp = i
-                break
-        
-        for i in relation[self.x]:
-            if i != tmp:
-                boys[i].reject()
-
-            relation[self.x].remove(i)
-        
-        relation[self.x].append(tmp)
-
-    
-    def choose_boy(self):
-        return self.order[0]
-    
     def reject(self):
         self.order.pop(0)
+        self.married = False
+
+    def propose(self):
+        self.married = True
+        relations[self.choose_proposee()].append(self.index)
 
 
-def stable_conditon():
-    for i in range(girls_num):
-        if len(relation[i]) > 1:
-            return i
-    
-    return -1
+class Proposee:
+    def __init__(self, index, preference_order):
+        self.index = index
+        self.order = sort_preferences(preference_order)
+
+    def compare_proposers(self):
+        relations[self.index].sort(key=self.order.index)
+        for proposer in relations[self.index][1:]:
+            proposers[proposer].reject()
+        relations[self.index] = [relations[self.index][0]]
+
+
+def sort_preferences(preference_order):
+    return sorted(range(len(preference_order)), key=preference_order.__getitem__)
+
+
+def get_wanted_proposees():
+    wanted_proposees = []
+    for i in range(proposees_count):
+        if len(relations[i]) > 1:
+            wanted_proposees.append(i)
+    return wanted_proposees
 
 
 def show_result():
-    for i in range(girls_num):
-        print(f'boy {relation[i]} --> girl {i}')
+    for i in range(proposees_count):
+        print(f'Boy {relations[i][0]+1} <--> Girl {i+1}')
 
 
+boys_count = int(input('Enter number of boys: '))
+girls_count = int(input('Enter number of girls: '))
+boy_optimal = bool(input('0. Girl Optimal 1.Boy Optimal: '))
+proposers = []
+proposees = []
+if boy_optimal:
+    proposers_count = boys_count
+    proposees_count = girls_count
+else:
+    proposers_count = girls_count
+    proposees_count = boys_count
+relations = [[] for _ in range(proposees_count)]
 
-############ main
-print('Enter boys prefrences')
-
-for i in range(boys_num):
-    list_g = [int(x) for x in input().split()]
-    boy = Boy(i, list_g)
-    boys.append(boy)
-
-print('Enter girls prefrences')
-
-for i in range(girls_num):
-    list_b = [int(x) for x in input().split()]
-    girl = Girl(i, list_b)
-    girls.append(girl)
-
-
-for ctr in range((boys_num * girls_num) + 1):
-    for i in boys:
-        if i.state == 0:
-            i.request()
-    
-    famouse_girl = stable_conditon()
-    # print("f_g", famouse_girl)
-    if(famouse_girl != -1):
-        # print('im here')
-        # for k in range(girls_num):
-            # print(f'{relation[k]}-{k}')
-        girls[famouse_girl].compare_boys()
-    
+print('Enter boys prefrences:')
+for i in range(boys_count):
+    preference_order = [int(x)-1 for x in input().split()]
+    if boy_optimal:
+        proposers.append(Proposer(i, preference_order))
     else:
-        print(ctr)
+        proposees.append(Proposee(i, preference_order))
+
+print('Enter girls prefrences:')
+for i in range(girls_count):
+    preference_order = [int(x)-1 for x in input().split()]
+    if boy_optimal:
+        proposees.append(Proposee(i, preference_order))
+    else:
+        proposers.append(Proposer(i, preference_order))
+
+
+for _ in range((proposers_count * proposees_count) + 1):
+    for proposer in proposers:
+        if not proposer.married:
+            proposer.propose()
+    wanted_proposees = get_wanted_proposees()
+    for proposee in wanted_proposees:
+        proposees[proposee].compare_proposers()
+    if not wanted_proposees:
         show_result()
         break
